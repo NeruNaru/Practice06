4edf6300d13339cd6d481604bcd029ba430e77a5
NeruNaru <nerunaru430@gmail.com>
2025-05-12 17:56:07 +0900

    5월 12일

--- src/com/javaex/ex03/Shape.java
+++ src/com/javaex/ex03/Shape.java
@@ -1,15 +1,21 @@
 package com.javaex.ex03;
 
 public class Shape {
+	//field
+	protected String fillColor;
+	protected String lineColor;
 	
-	private String fillColor;
-	private String lineColor;
+	//editor
+	public Shape() {
+		
+	}
 	
 	public Shape(String fillColor, String lineColor) {
 		this.fillColor = fillColor;
 		this.lineColor = lineColor;
 	}
-
+	
+	//method g/s
 	public String getFillColor() {
 		return fillColor;
 	}
@@ -25,7 +31,7 @@ public class Shape {
 	public void setLineColor(String lineColor) {
 		this.lineColor = lineColor;
 	}
-	
+	//method normal
 	
 	
 	
--- src/com/javaex/ex04/Employee.java
+++ src/com/javaex/ex04/Employee.java
@@ -2,13 +2,18 @@ package com.javaex.ex04;
 
 public class Employee {
 	//field
-	private int salary;S
+	private int salary;
 	//editor
 	public Employee(int salary) {
 		this.salary = salary;
 	}
 	//method g/s
-	
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 	//method normal
 	
 	
